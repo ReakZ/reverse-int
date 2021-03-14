cae31ac9338b587542ee867ea2559d9e4224a0b0
RealZ <12774452+ReakZ@users.noreply.github.com>
2021-03-14 11:42:18 +0300

    feat: implement reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let string = Math.abs(n).toString();
+    let reverseNumber = string.split("").reverse().join("");
+    return parseInt(reverseNumber, 10);
+};
